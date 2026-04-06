dd6061586818173796a774eea1990e6c3df61541
Gerald G <geraldguignard2@gmail.com>
2026-04-06 15:11:48 -0400

    Added a message about potatoes.

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello I am Gerald!");
+        System.out.println("Potatoes!");
     }
 }
 
